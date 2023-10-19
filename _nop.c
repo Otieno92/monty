c903c2be46cbd1fb760acc91b8b7fb6ce294ad0f
Otieno92 <otienofredricko409@gmail.com>
2023-10-19 13:18:21 -0700

    opcode

---
+++ _nop.c
@@ -0,0 +1,17 @@
+#include  "monty.h"
+
+/*
+ * f_nop - The nop opcode  doesn’t do anything.
+ *
+ *
+ * @head: the head of the stack
+ * @counter: counts the line_number
+ *
+ * Return: nothing
+ */
+
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
